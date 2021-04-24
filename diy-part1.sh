3998d74a25e96c2ee34c7a10815c93ea4e4ef6b2
mz520k <82831770+mz520k@users.noreply.github.com>
2021-04-24 21:43:06 +0800

    Add files via upload

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,20 +1,17 @@
-echo "Apply custom.sh"
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
 
-source ../version
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-rm -rf package/lean/default-settings/files/zzz-default-settings
-wget https://raw.githubusercontent.com/coolsnowwolf/lede/99222f49fd9f63eef575ac34f251280e6e97f3b3/package/lean/default-settings/files/zzz-default-settings -O package/lean/default-settings/files/zzz-default-settings
-sed -i 's/DEPENDS.*/& \+luci-i18n-samba-zh-cn/g'  package/lean/autosamba/Makefile
-
-sed -i '92d'                                                                   package/system/opkg/Makefile
-sed -i '/shadow/d'                     package/lean/default-settings/files/zzz-default-settings
-sed -i '/nas/d'                     package/lean/default-settings/files/zzz-default-settings
-sed -i "s/openwrt.proxy.ustclug.org/openwrt.download/g"  package/lean/default-settings/files/zzz-default-settings
-sed -i "s/https:/R20.0.0/g"  package/lean/default-settings/files/zzz-default-settings
-sed -i  's/http:/snapshots/g'  package/lean/default-settings/files/zzz-default-settings
-sed -i  " 23i sed -i 's/http:/https:/g' /etc/opkg/distfeeds.conf"  package/lean/default-settings/files/zzz-default-settings
-sed -i  "s/R20\(.[0-9].[0-9]\{1,2\}\)/R21.$version/g" package/lean/default-settings/files/zzz-default-settings
-
-
-sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' > /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
-sed -i "45i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/x86_64' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
