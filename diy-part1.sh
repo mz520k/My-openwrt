69ba09aed8f1c6de6f9410bdc1b2be8d8c2017d3
mz520k <82831770+mz520k@users.noreply.github.com>
2021-04-25 19:08:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i "44i echo 'src/gz openwrt_all https://openwrt.download/ExtraPackages/all' > /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
+sed -i "45i echo 'src/gz openwrt_ipk https://openwrt.download/ExtraPackages/x86_64' >> /etc/opkg/customfeeds.conf " package/lean/default-settings/files/zzz-default-settings
