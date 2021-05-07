3fd0627c513d146bc738ac27656a3da2a87af983
mz520k <82831770+mz520k@users.noreply.github.com>
2021-05-07 18:14:09 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
-# svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
+svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns-le package/smartdns-le
